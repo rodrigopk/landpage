0173407bfb736745ab0686d498a454098835aebd
Rodrigo Vasconcelos <rodrigo.vasconcelos@ifactory.com.br>
2017-06-14 17:07:43 -0300

    updating user serializer

--- app/serializers/developer_serializer.rb
+++ app/serializers/developer_serializer.rb
@@ -1,3 +1,6 @@
 class DeveloperSerializer < ActiveModel::Serializer
   attributes :id, :name, :age, :location, :title, :picture, :pitch
+  has_many :skills
+  has_many :interests
+  has_many :experiences
 end
