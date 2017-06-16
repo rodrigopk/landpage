27e62712e74f107f113fdf3b22313128e861c06a
rodrigopk <rodrigopk>
2017-06-15 21:08:27 -0300

    fixing buttons on experience list

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,13 @@ Developer.create( name: "Rodrigo Vasconcelos de Barros",
                   title: "Fullstack developer",
                   birthdate:Date.new(1988,4,7),
                   location: "Fortaleza, CE, Brazil",
+                  picture: File.new("public/selfie.jpg"),
                   pitch:"I’m a software developer with a passion for solving problems and programming"+
                         " as a tool to make people’s lives easier.\nI graduated from IFCE - Ceará’s Federal"+
                         " Institute of Technology and Education in 2016, and have been working on software"+
                         " development for mobile and web for two years. I also have some experience developing"+
                         " desktop and embedded applications.\nMost recently I have been working with Ruby on Rails"+
-                        " development, focusing on building RESTful API’s and designing ")
+                        " development, focusing on building RESTful API’s and designing scalable backend structure.")
 
 Developer.first.experiences.create( [
   { title: "Fullstack Developer",
