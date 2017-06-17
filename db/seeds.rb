f5f7152aabf8a26cb5e7496d4db9a3e1c1cf5c85
rodrigopk <rodrigopk>
2017-06-17 10:13:08 -0300

    using external images in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ Developer.create( name: "Rodrigo Vasconcelos de Barros",
                   title: "Fullstack developer",
                   birthdate:Date.new(1988,4,7),
                   location: "Fortaleza, CE, Brazil",
-                  picture: File.new("public/selfie.jpg"),
+                  picture: URI.parse("http://res.cloudinary.com/rodrigopk/image/upload/v1497703499/1/original/selfie.jpg"),
                   pitch:"I’m a software developer with a passion for solving problems and programming"+
                         " as a tool to make people’s lives easier.\nI graduated from IFCE - Ceará’s Federal"+
                         " Institute of Technology and Education in 2016, and have been working on software"+
@@ -53,40 +53,40 @@ Developer.first.experiences.create( [
 Developer.first.skills.create([
   { title: "Ruby on Rails",
     experience: 24,
-    image: File.new("public/icons/rails.png"),
+    image: URI.parse("http://res.cloudinary.com/rodrigopk/image/upload/v1497703501/1/original/rails.png"),
     description: "I have worked with RoR since version 4.2, mostly using it to create RESTful APIs.\n"+
                   "Fun fact: this site uses a rails 5.1 API to feed it's data."
   },
   { title: "angularJS",
     experience: 8,
-    image: File.new("public/icons/angularjs.png"),
+    image: URI.parse("http://res.cloudinary.com/rodrigopk/image/upload/v1497703502/2/original/angularjs.png"),
     description: "I have used angularJS  (both v1 and v2) to build client side applications,"+
                   " both professionally and on personal projects.\nFun fact: this app uses angularJS "+
                   "v2 on the frontend."
   },
   { title: "Swift",
     experience: 18,
-    image: File.new("public/icons/swift.png"),
+    image: URI.parse("http://res.cloudinary.com/rodrigopk/image/upload/v1497703504/3/original/swift.png"),
     description: "I have used Swift to program for iOS devices since it's debut on 2014."+
                 "I have developed 4 apps using it, during my time on BEPiD and after"+
                 "it's conclusion."
   },
   { title: "C++",
     experience: 12,
-    image: File.new("public/icons/c++.png"),
+    image: URI.parse("http://res.cloudinary.com/rodrigopk/image/upload/v1497703506/4/original/c__.png"),
     description: "I have used C++ to develop desktop applications both professionally and"+
                   " academically, but haven't done it in a while. Not a stranger to scrubbing bits, though."
   },
   { title: "Git",
     experience: 36,
-    image: File.new("public/icons/git.png"),
+    image: URI.parse("http://res.cloudinary.com/rodrigopk/image/upload/v1497703508/5/original/git.png"),
     description: "I use git as my version control software for most of my projects, and It's "+
                   " also the one I have used the most professionally. I prefer using it via command"+
                   "line, for practicality."
   },
   { title: "Scrum",
     experience: 36,
-    image: File.new("public/icons/scrum.png"),
+    image: URI.parse("http://res.cloudinary.com/rodrigopk/image/upload/v1497703510/6/original/scrum.png"),
     description: "I have used used scrum for most of the projects I worked with other developers, in"+
                   "projects with different scopes and teams both large and small."
   }
