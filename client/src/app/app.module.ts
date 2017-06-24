63ddcf373c622dc14ff88d1c06345d43cb8f3736
rodrigopk <rodrigopk>
2017-06-24 16:00:21 -0300

    implementing header component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {ScrollToModule} from 'ng2-scroll-to';
 import { AppComponent } from './app.component';
 import { DeveloperCardComponent } from './developer-card/developer-card.component';
 import { ExperienceListComponent } from './experience-list/experience-list.component';
@@ -11,6 +12,7 @@ import { InterestListComponent } from './interest-list/interest-list.component';
 import { EducationListComponent } from './education-list/education-list.component';
 import { ContactCardComponent } from './contact-card/contact-card.component';
 import { PhonePipe } from './phone.pipe';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -21,12 +23,14 @@ import { PhonePipe } from './phone.pipe';
     InterestListComponent,
     EducationListComponent,
     ContactCardComponent,
-    PhonePipe
+    PhonePipe,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ScrollToModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
